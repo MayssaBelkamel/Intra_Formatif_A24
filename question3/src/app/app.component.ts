f769f808c8224916610119fdde233d94e8345ff4
MayssaBelkaml <2483601@cegepmontpetit.ca>
2026-03-11 22:47:02 -0400

    debut question 3 formatif

--- question3/src/app/app.component.ts
+++ question3/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { AbstractControl, FormBuilder, ValidationErrors, Validators, ReactiveFormsModule } from '@angular/forms';
+import { AbstractControl, FormBuilder, ValidationErrors, Validators, ReactiveFormsModule, FormGroup } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -13,16 +13,27 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
     standalone: true,
-    imports: [MatToolbarModule, MatIconModule, MatCardModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule]
+    imports: [MatToolbarModule,
+       MatIconModule,
+        MatCardModule,
+         ReactiveFormsModule,
+          MatFormFieldModule,
+           MatInputModule]
 })
 export class AppComponent {
-  title = 'reactive.form';
-
-  
-
-  constructor(
-    
-  ) { }
+ formGroup: FormGroup;
+  constructor(private formBuilder: FormBuilder) {
+        this.formGroup = this.formBuilder.group(
+      {
+        nom: ['', [Validators.required]],
+        roadNumber : ['', [Validators.required],
+                          Validators.min(1000), // Minimum allowed value is 10
+                         Validators.max(9999) ],
+        postalCode: ['', [ Validators.pattern('"^[A-Z][0-9][A-Z][ ]?[0-9][A-Z][0-9]$')]],
+        comment: ['', [Validators.required]]
+      },
+    );
+  }
 }
 
 
